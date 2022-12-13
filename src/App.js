f37cc8fc6fc8fdd15a8095e237004098dbb88560
EmirlanDogdurbaev <jigit38@gmail.com>
2022-12-13 10:13:49 +0600

    added bg image and styled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Navigation/Header/Header";
 import Notes from "./components/Notes/Notes";
 import Profile from "./components/Profile/Profile";
 import Favorites from "./components/Favorites/Favorites";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="profile" element={<Profile />}></Route>
           <Route path="notes" element={<Notes />}></Route>
           <Route path="favorites" element={<Favorites />}></Route>
